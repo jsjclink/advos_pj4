ef4615a133e4231e959d2809b7a9020ab40aa91f
jsjclink <jsjclink@naver.com>
2024-04-20 07:22:45 +0000

    manager - storage connection

--- gtstore/src/manager.cpp
+++ gtstore/src/manager.cpp
@@ -1,9 +1,12 @@
 #include "gtstore.hpp"
 #include <grpc/grpc.h>
+#include <grpcpp/channel.h>
 #include <grpcpp/security/server_credentials.h>
 #include <grpcpp/server.h>
 #include <grpcpp/server_builder.h>
 #include <grpcpp/server_context.h>
+#include <grpcpp/client_context.h>
+#include <grpcpp/create_channel.h>
 
 #include <mutex>
 #include <vector>
@@ -20,8 +23,34 @@ using keyvaluestore::KeyValue;
 using keyvaluestore::Void;
 using keyvaluestore::Port;
 
+using grpc::Channel;
+using grpc::ClientContext;
+
 std::vector<int64_t> node_vector;
 std::mutex node_vector_mtx;
+
+class KeyValueServiceStorageClient {
+	public:
+	KeyValueServiceStorageClient(std::shared_ptr<Channel> channel) : stub_(KeyValueService::NewStub(channel)) {
+
+	}
+
+	void check_alive() {
+		ClientContext context;
+		Void request, response;
+
+		Status status = stub_->check_alive(&context, request, &response);
+
+		if(status.ok()) {
+			cout << "check_alive OK\n";
+		} else {
+			cout << "check_alive Not OK\n";
+		}
+	}
+
+	private:
+	std::unique_ptr<KeyValueService::Stub> stub_;
+};
 
 class KeyValueServiceManagerImpl final : public KeyValueService::Service {
 	public:
@@ -38,10 +67,16 @@ class KeyValueServiceManagerImpl final : public KeyValueService::Service {
 	}
 
 	Status str_cnt(ServerContext* context, const Port* port, Void* response) override {
-		cout << "str_cnt called by storage node. port num: " << port->port() << "\n";
+		cout << "str_cnt is called by storage node. port num: " << port->port() << "\n";
 		cout << "connect to storage node. port num: " << port->port() << "\n";
 
-		
+		// connect to storage node
+		std::string server_address = "0.0.0.0:" + std::to_string(port->port());
+		cout << server_address << "\n";
+		KeyValueServiceStorageClient client(grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()));
+
+		// call check alive for test
+		client.check_alive();
 
 		std::lock_guard<std::mutex> lock(node_vector_mtx);
     	node_vector.push_back(port->port());
--- gtstore/src/storage.cpp
+++ gtstore/src/storage.cpp
@@ -32,6 +32,31 @@ Port MakePort(int64_t port) {
 	return p;
 }
 
+class KeyValueServiceManagerClient {
+	public: 
+	KeyValueServiceManagerClient(std::shared_ptr<Channel> channel) : stub_(KeyValueService::NewStub(channel)) {
+
+	}
+
+	void str_cnt(int64_t port_int) {
+		ClientContext context;
+		Port port = MakePort(port_int);
+		Void response;
+
+		Status status = stub_->str_cnt(&context, port, &response);
+
+		if(status.ok()) {
+			cout << "str_cnt OK\n";
+		} else {
+			cout << "str_cnt Not OK\n";
+		}
+	}
+
+	private:
+	std::unique_ptr<KeyValueService::Stub> stub_;
+};
+
+
 class KeyValueServiceStorageImpl final : public KeyValueService::Service {
 	public:
 	KeyValueServiceStorageImpl() {
@@ -47,33 +72,11 @@ class KeyValueServiceStorageImpl final : public KeyValueService::Service {
 	}
 
 	Status check_alive(ServerContext* context, const Void* request, Void* response) {
+		cout << "check_alive is called by storage node.\n";
+		
 		return Status::OK;
 	}
 };
-
-class KeyValueServiceManagerClient {
-	public: 
-	KeyValueServiceManagerClient(std::shared_ptr<Channel> channel) : stub_(KeyValueService::NewStub(channel)) {
-
-	}
-	void str_cnt(int64_t port_int) {
-		ClientContext context;
-		Port port = MakePort(port_int);
-		Void response;
-
-		Status status = stub_->str_cnt(&context, port, &response);
-
-		if(status.ok()) {
-			cout << "OK\n";
-		} else {
-			cout << "Not OK\n";
-		}
-	}
-
-	private:
-	std::unique_ptr<KeyValueService::Stub> stub_;
-};
-
 
 void GTStoreStorage::init() {
 	cout << "Inside GTStoreStorage::init()\n";
@@ -86,13 +89,19 @@ void GTStoreStorage::init() {
   	builder.RegisterService(&service);
   	std::unique_ptr<Server> server(builder.BuildAndStart());
   	std::cout << "Server listening on " << server_address << std::endl;
+
+	// connect to manager
+	cout << "connect to server\n";
+	KeyValueServiceManagerClient client(grpc::CreateChannel("0.0.0.0:50051", grpc::InsecureChannelCredentials()));
+	client.str_cnt(50052);
+
   	server->Wait();
 }
 
 int main(int argc, char **argv) {
-	KeyValueServiceManagerClient client(grpc::CreateChannel("0.0.0.0:50051", grpc::InsecureChannelCredentials()));
-	client.str_cnt(50052);
+	// todo: argv -> port
 
+	// start server
 	GTStoreStorage storage;
 	storage.init();
 }
