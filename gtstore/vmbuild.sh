71ef74df7158c1f0c07101ce47144b262a1a1f03
croc937 <croc937@kaist.ac.kr>
2024-04-23 09:39:14 -0500

    Added vmbuild.sh

---
+++ gtstore/vmbuild.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+export MY_INSTALL_DIR=$HOME/.local
+mkdir -p $MY_INSTALL_DIR
+export PATH="$MY_INSTALL_DIR/bin:$PATH"
+wget -q -O cmake-linux.sh https://github.com/Kitware/CMake/releases/download/v3.19.6/cmake-3.19.6-Linux-x86_64.sh
+sh cmake-linux.sh -- --skip-license --prefix=$MY_INSTALL_DIR
+rm cmake-linux.sh
+cd ../
+git clone --recurse-submodules -b v1.62.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc
+cd grpc
+mkdir -p cmake/build
+pushd cmake/build
+cmake -DgRPC_INSTALL=ON \
+      -DgRPC_BUILD_TESTS=OFF \
+      -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
+      ../..
+make -j 4
+make install
+popd
+cd ../gtstore
+./build.sh
